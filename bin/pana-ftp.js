bfda69d11fdbe2fd556279a0b27f297cbd813352
Pace Zhong <pace_zhong@foxmail.com>
2015-02-27 18:39:33 +0800

    开始ftp功能实现

--- bin/pana-ftp.js
+++ bin/pana-ftp.js
@@ -1,4 +1,21 @@
-var color=require('colorful')
-module.exports = function(config) {
-  console.log('upload to ftp:location....'+color.green('success'));
+var color=require('colorful'),
+    fs=require('fs');
+
+
+module.exports = function(path) {
+  fs.readFile('pana-config.json','utf8',function(error,data){
+    if(!error){
+      var ftpConfig=JSON.parse(data).ftpSync,
+
+      //获取相对路径
+      currentRoot=process.cwd(),
+      relativePath=path.replace(currentRoot,'').replace(/\\/g,'/'),
+      uploadPath=ftpConfig.host+relativePath;
+
+      console.log('upload '+color.bold(path)+' to '+color.bold(uploadPath)+" -- "+color.green('success'));
+    }else{
+      console.log(color.red("Cann't found pana-config.json, Please Ctrl+C Stop!"));
+    }
+  });
 };
+
